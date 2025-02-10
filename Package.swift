18bdaaeaf3d5665f95983c984b22463cf084a358
AlisaMylnikova <alisa.mylnikova@exyte.com>
2025-02-10 16:13:23 +0700

    Rename package

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SolanaExyteFork",
-            targets: ["Solana"])
+            targets: ["SolanaExyteFork"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,7 +24,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Solana",
+            name: "SolanaExyteFork",
             dependencies: ["TweetNacl", "Starscream", "secp256k1", "Beet"]
         ),
         .testTarget(
