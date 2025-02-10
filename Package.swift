94c639c6b2d9bb3bde68e970c6a988054fdc1a87
AlisaMylnikova <alisa.mylnikova@exyte.com>
2025-02-10 16:04:30 +0700

    Rename package

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Solana",
+    name: "SolanaExyteFork",
     platforms: [.iOS(.v11), .macOS(.v10_12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Solana",
+            name: "SolanaExyteFork",
             targets: ["Solana"])
     ],
     dependencies: [
